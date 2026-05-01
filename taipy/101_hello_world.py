9edc44955822eec718a029809976c176f280ab74
nockchun <nockchun@gmail.com>
2026-05-01 15:47:04 +0900

    create base structure

--- taipy/101_hello_world.py
+++
@@ -1,8 +0,0 @@
-###############################################################################
-# Base Taipy GUI Application.
-###############################################################################
-from taipy import Gui
-
-page = "# Hello Taipy !"
-
-Gui(page).run(port=5050, debug=True, use_reloader=True, title="step 101")
